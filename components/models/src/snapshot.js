1f3395a125446fe2f3a40c2d4ffc5b2e52956f00
Martin Samami ッ <martin@digitalkookie.io>
2016-09-22 18:28:39 +0200

    Use mediumtext instead of text

--- components/models/src/snapshot.js
+++ components/models/src/snapshot.js
@@ -9,7 +9,7 @@
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU General Public License for more details.te
  *
  * You should have received a copy of the Mozilla Public License
  * along with this program.  If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
@@ -29,10 +29,10 @@ module.exports = {
     }
 
     // the changeset
-  , changes: 'text'
+  , changes: 'mediumtext'
 
     // the content resulting from the changes
-  , contents: 'text'
+  , contents: 'mediumtext'
 
     // belongsTo a document
   , document: {
